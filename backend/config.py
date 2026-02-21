521fb554aabfefb7630ad8cb865b16674c076c19
Anastasia <cookiedemons@outlook.com>
2026-02-21 03:34:07 -0500

    refactor: removing yagni

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
-OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "llama3.2:1b")
